800092d6dcb1374dbfb82c4114b7ce556759bbbf
Chris Hayen <chris@shotgun.dev>
2026-02-14 18:00:12 -0500

    feat(seeds): create default admin with forced password reset

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,25 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     Valkyrie.Repo.insert!(%Valkyrie.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+alias Valkyrie.Accounts.User
+alias Valkyrie.Repo
+
+admin_email = "chris@shotgun.dev"
+admin_password = "changeme"
+
+admin_attrs = %{
+  email: admin_email,
+  hashed_password: Bcrypt.hash_pwd_salt(admin_password),
+  confirmed_at: DateTime.utc_now(:second),
+  is_admin: true,
+  must_change_password: true
+}
+
+case Repo.get_by(User, email: admin_email) do
+  nil ->
+    %User{}
+    |> Ecto.Changeset.change(admin_attrs)
+    |> Repo.insert!()
+
+  user ->
+    user
+    |> Ecto.Changeset.change(admin_attrs)
+    |> Repo.update!()
+end
